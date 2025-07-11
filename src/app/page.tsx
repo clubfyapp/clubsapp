d4acee02cb3f9ab3669130cd8f69ae7ad069e3b9
O001830 <fernando.aguilar.santos@bbva.com>
2025-07-11 23:19:29 +0200

    raro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client"; // Marca este archivo como un Client Component
+
 import Image from "next/image";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
